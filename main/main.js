8d6bf4d39778d0e4aa9c8fcf7955f9b6672814ea
ZZZZone <871532895@qq.com>
2019-05-24 22:07:00 +0800

    submit my result

--- main/main.js
+++ main/main.js
@@ -1,4 +1,24 @@
-module.exports = function main() {
-    console.log("Debug Info");
-    return 'Hello World!';
+module.exports = function main(str) {
+    const mapTop =    [`._.`, `...`, `._.`, `._.`, `...`, `._.`, `._.`, `._.`, `._.`, `._.`]
+  	const mapMid =    [`|.|`, `..|`, `._|`, `._|`, `|_|`, `|_.`, `|_.`, `..|`, `|_|`, `|_|`]
+  	const mapBottom = [`|_|`, `..|`, `|_.`, `._|`, `..|`, `._|`, `|_|`, `..|`, `|_|`, `..|`]
+
+  	var ansTop = ""
+  	var ansMid = ""
+  	var ansBottom = ""
+
+  	for (var i = 0; i < str.length; i++) {
+  		ansTop += mapTop[str[i]]
+  		ansMid += mapMid[str[i]]
+  		ansBottom += mapBottom[str[i]]
+  		if(i != str.length-1){
+  			ansTop += " "
+  			ansMid += " "
+  			ansBottom += " "
+  		}
+  	}
+  	ansTop += "\n"
+  	ansMid += "\n"
+  	ansBottom += "\n"
+  	return ansTop + ansMid + ansBottom
 };
